bbc3b319d0610f2406f6da27cbbe0bb66b7e54e4
Luiz <luizmoraesaf@gmail.com>
2025-08-14 18:24:14 -0300

    feat: Refactor code structure for improved readability and maintainability

--- src/constant/index.tsx
+++ src/constant/index.tsx
@@ -2,13 +2,14 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home";
 import NotFound from "../pages/not-found";
 import AppShell from "../components/layout/app-shell";
+import { ROUTES } from "./routes";
 
 const Navigation = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppShell />} path="/">
-          <Route path="/home" element={<Home />} />
+          <Route path={ROUTES.home} element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
