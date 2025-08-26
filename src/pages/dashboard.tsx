1792684165ff8b6c34c4f9488f91da017fb5f51d
Mateus <mateus.campos@poatek.com>
2025-08-26 13:52:02 -0300

    add: pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="container py-10">
+      <h1 className="text-3xl font-semibold mb-4">Página de Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="container py-10">
+      <h1 className="text-3xl font-semibold mb-4">Página de Login</h1>
+    </div>
+  );
+};
+
+export default Login;
