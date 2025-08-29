e47495b3e5d5b34cac8648739dafe6fb83d8cc9e
Mateus <mateus.campos@poatek.com>
2025-08-29 11:15:09 -0300

    add: todos os avatares em formato circular

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -3,9 +3,9 @@ import { cva, type VariantProps } from "class-variance-authority";
 const avatarVariants = cva("", {
   variants: {
     size: {
-      small: "h-6 w-6 rounded-xl",
-      medium: "h-10 w-10 rounded-[20px]",
-      large: "h-[100px] w-[100px] rounded-[15px]",
+      small: "h-6 w-6 rounded-[100%]",
+      medium: "h-10 w-10 rounded-[100%]",
+      large: "h-[100px] w-[100px] rounded-[100%]",
     },
   },
 });
