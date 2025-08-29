199facabb8b850a9486a2128b4f603ff4c308b39
Artur Schultz <artur1schultz@gmail.com>
2025-08-29 10:57:31 -0300

    fix:  merge conflicts

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,28 +1,19 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import * as React from "react"
 
+const avatarVariants = cva("", {
+  variants: {
+    size: {
+      small: "h-6 w-6 rounded-xl",
+      medium: "h-10 w-10 rounded-[20px]",
+      large: "h-[100px] w-[100px] rounded-[15px]",
+    },
+  },
+});
 
-const avatarVariants = cva(
-    "",
-    {
-        variants: {
-            size: {
-                small: "h-6 w-6 rounded-xl",
-                medium: "h-10 w-10 rounded-[20px]",
-                large: "h-[100px] w-[100px] rounded-[15px]"
-            }
-        }
-    }
-)
-
-interface AvatarProps extends VariantProps<typeof avatarVariants>{
-    imgUrl: string;
+interface AvatarProps extends VariantProps<typeof avatarVariants> {
+  imgUrl: string;
 }
 
-
-
-export function Avatar({imgUrl, size = "medium"} :AvatarProps){
-    return (
-        <img src={imgUrl} className={avatarVariants({size})}/>
-    )
+export function Avatar({ imgUrl, size = "medium" }: AvatarProps) {
+  return <img src={imgUrl} className={avatarVariants({ size })} />;
 }
