8ce4015611ef7a5630214dc0ebfdc9f9db95e0a2
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-06 18:30:38 -0300

    feat: create SearchBar component

--- src/components/layout/search-bar.tsx
+++ src/components/layout/search-bar.tsx
@@ -1,11 +1,27 @@
+import { useDebounce } from "@/hooks/useDebounce";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Search } from "lucide-react";
 import Input from "../ui/input";
 
-import { Search } from "lucide-react";
-
 export const SearchBar = () => {
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (debouncedValue) {
+      setSearchParams({ search: debouncedValue });
+    }
+  }, [debouncedValue, setSearchParams]);
+
   return (
     <div>
-      <Input RightIcon={<Search className="h-4 w-4" color="#94A3B8" />} />
+      <Input
+        RightIcon={<Search className="h-4 w-4" color="#94A3B8" />}
+        onChange={(e) => setValue(e.currentTarget.value)}
+      />
     </div>
   );
 };
