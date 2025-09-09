59bdc7024d4c9fb62b6cfd6dddbca8bdbe0c363a
Gabriel Camargo Bittencourt <bittencourt.gabriel@edu.pucrs.br>
2025-09-09 01:02:44 +0000

    Search Bar component

---
+++ src/components/layout/search-bar.tsx
@@ -0,0 +1,28 @@
+import { useDebounce } from "@/hooks/useDebounce";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Search } from "lucide-react";
+import Input from "../ui/input";
+
+export const SearchBar = () => {
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (debouncedValue) {
+      setSearchParams({ search: debouncedValue });
+    }
+  }, [debouncedValue, setSearchParams]);
+
+  return (
+    <div>
+      <Input
+        RightIcon={<Search className="h-4 w-4" color="#94A3B8" />}
+        onChange={(e) => setValue(e.currentTarget.value)}
+        placeholder="Buscar"
+      />
+    </div>
+  );
+};
