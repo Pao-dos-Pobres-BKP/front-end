624eeed5243a9eed80df4a02b66df3af42c89c6e
Artur Schultz <artur1schultz@gmail.com>
2025-09-23 19:05:04 -0300

    Fix: fix types in formatCurrency function

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,6 +1,10 @@
-export function formatCurrency(value: string) {
-    const cleaned = value
-    .replace(/\D/g,'');
+export function formatCurrency(value: string | number) {
+    let cleaned = value;
+
+    if(typeof value == "string"){
+        cleaned = (cleaned as string).replace(/\D/g,'')
+    }
+    
     return new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL", minimumFractionDigits: 2 }).format(
         Number(cleaned)/100
     );
