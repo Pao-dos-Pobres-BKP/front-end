3bd22705f5f61d937481f1c39e45c2f665238c82
Mateus Campos Caçabuena <mateus.cacabuena@edu.pucrs.br>
2025-09-27 03:18:57 +0000

    Refatorando o botão e outros ajustes gerais

--- src/components/ui/newsletter.tsx
+++ src/components/ui/newsletter.tsx
@@ -32,9 +32,7 @@ export const Newsletter = () => {
 
   return (
     <div className="p-6 flex flex-col gap-6 w-full bg-[#CCE0E6]">
-      <h3 className="uppercase text-[32px] leading-[110%] font-bold">
-        Fique por dentro do futuro
-      </h3>
+      <h3 className="uppercase text-[32px] leading-[110%] font-bold">Fique por dentro do futuro</h3>
       <p className="text-center max-w-[800px] mx-auto font-bold md:leading-[150%]">
         A newsletter do Pão dos Pobres é um canal de conexão entre você e nossas ações. Nela,
         compartilhamos histórias de transformação, resultados de impacto, campanhas em andamento e
@@ -42,7 +40,7 @@ export const Newsletter = () => {
         crianças, adolescentes e jovens em situação de vulnerabilidade.
       </p>
       <div className="flex flex-col items-center w-full max-w-md gap-4 mx-auto">
-        <div className="flex w-full gap-2 items-center">
+        <div className="flex flex-col sm:flex-row sm:gap-2 w-full gap-4 items-center">
           <Input
             placeholder="Email"
             fullWidth
@@ -51,7 +49,7 @@ export const Newsletter = () => {
             className="h-12 w-full"
           />
           <Button
-            size="extraSmall"
+            size="small"
             disabled={!email || !isChecked}
             onClick={handleSubmit}
             variant="quinary"
@@ -68,19 +66,13 @@ export const Newsletter = () => {
           checked={isChecked}
         />
 
-        {errorMessage && (
-          <span className="text-red-500 text-sm">{errorMessage}</span>
-        )}
+        {errorMessage && <span className="text-red-500 text-sm">{errorMessage}</span>}
       </div>
 
       <Modal
         title="Newsletter assinada!"
         footer={
-          <Button
-            variant="tertiary"
-            size="extraSmall"
-            onClick={() => setIsModalOpen(false)}
-          >
+          <Button variant="tertiary" size="extraSmall" onClick={() => setIsModalOpen(false)}>
             Fechar
           </Button>
         }
