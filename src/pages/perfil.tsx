627a3192f487705ef8afee50edab3eb4ddb6d99f
JPSouzaSilva <jpsouzadasilva@gmail.com>
2025-10-07 18:07:33 -0300

    aws

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -11,12 +11,23 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "@/components/ui/pagination";
 
 import { useState } from "react";
 import EditUserModal from "@/components/ui/edit-user-modal";
 import ConfirmLogoutModal from "@/components/ui/confirm-logout-modal";
 
+export interface User {
+  nome: string;
+  nascimento: string;
+  genero: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+  totalDonated: number;
+  percentageAchieved: number;
+  foto: string;
+}
 
 export default function Perfil() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -24,7 +35,7 @@ export default function Perfil() {
   const [currentPage, setCurrentPage] = useState(1);
   const cardsPerPage = 6;
 
-  const [dados, setDados] = useState({
+  const [dados, setDados] = useState<User>({
     nome: "Fulano de Tal",
     nascimento: "12 de Agosto de 1971",
     genero: "Masculino",
@@ -73,16 +84,70 @@ export default function Perfil() {
     goal: number;
     situation: SituationType;
   }[] = [
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 81825.33, goal: 90000, situation: "recurring" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 5000, goal: 10000, situation: "recurring" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 15000, goal: 20000, situation: "approved" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 2500, goal: 3000, situation: "approved" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 7000, goal: 10000, situation: "approved" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 9000, goal: 15000, situation: "approved" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 1200, goal: 2000, situation: "approved" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 4500, goal: 5000, situation: "approved" },
-      { title: "Campanha de Santo Antônio", creatorName: " Fundação Pão dos Pobres Santo Antônio", raised: 800, goal: 1000, situation: "approved" },
-    ];
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 81825.33,
+      goal: 90000,
+      situation: "recurring",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 5000,
+      goal: 10000,
+      situation: "recurring",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 15000,
+      goal: 20000,
+      situation: "approved",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 2500,
+      goal: 3000,
+      situation: "approved",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 7000,
+      goal: 10000,
+      situation: "approved",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 9000,
+      goal: 15000,
+      situation: "approved",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 1200,
+      goal: 2000,
+      situation: "approved",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 4500,
+      goal: 5000,
+      situation: "approved",
+    },
+    {
+      title: "Campanha de Santo Antônio",
+      creatorName: " Fundação Pão dos Pobres Santo Antônio",
+      raised: 800,
+      goal: 1000,
+      situation: "approved",
+    },
+  ];
 
   const totalPages = Math.ceil(campanhasHistorico.length / cardsPerPage);
   const indexOfLastCard = currentPage * cardsPerPage;
@@ -93,7 +158,7 @@ export default function Perfil() {
     setIsModalOpen(true);
   };
 
-  const handleSalvarPerfil = (novosDados: any) => {
+  const handleSalvarPerfil = (novosDados: User) => {
     setDados(novosDados);
   };
 
@@ -155,17 +220,13 @@ export default function Perfil() {
                     </span>
                   </div>
                   <div className="flex items-center gap-2">
-                    <label className="text-sm font-medium text-[#005172] text-left">
-                      Gênero:
-                    </label>
+                    <label className="text-sm font-medium text-[#005172] text-left">Gênero:</label>
                     <span className="w-60 py-2 pl-0 pr-3 text-sm text-[#94A3B8] text-left">
                       {dados.genero}
                     </span>
                   </div>
                   <div className="flex items-center gap-2">
-                    <label className="text-sm font-medium text-[#005172] text-left">
-                      CPF:
-                    </label>
+                    <label className="text-sm font-medium text-[#005172] text-left">CPF:</label>
                     <span className="w-60 py-2 pl-0 pr-3 text-sm text-[#94A3B8] text-left">
                       {dados.cpf}
                     </span>
@@ -179,9 +240,7 @@ export default function Perfil() {
                     </span>
                   </div>
                   <div className="flex items-center gap-2">
-                    <label className="text-sm font-medium text-[#005172] text-left">
-                      E-mail:
-                    </label>
+                    <label className="text-sm font-medium text-[#005172] text-left">E-mail:</label>
                     <span className="w-60 py-2 pl-0 pr-3 text-sm text-[#94A3B8] text-left">
                       {dados.email}
                     </span>
@@ -192,18 +251,24 @@ export default function Perfil() {
                   <span className="text-sm text-[#005172] whitespace-nowrap">
                     Quanto doou até agora:
                   </span>
-                  <Progress value={dados.percentageAchieved} variant="blue" size="medium" className="flex-1" />
+                  <Progress
+                    value={dados.percentageAchieved}
+                    variant="blue"
+                    size="medium"
+                    className="flex-1"
+                  />
                   <span className="text-sm text-[#005172] whitespace-nowrap">
-                    {dados.totalDonated.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+                    {dados.totalDonated.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
                   </span>
                 </div>
               </div>
             </div>
 
             <div className="flex-1 flex flex-col gap-3 items-start">
-              <h3 className="text-sm font-semibold text-[#005172]">
-                Campanhas que apoia
-              </h3>
+              <h3 className="text-sm font-semibold text-[#005172]">Campanhas que apoia</h3>
 
               {campanhas.length > 0 ? (
                 campanhas.map((campanha, index) => (
@@ -227,16 +292,11 @@ export default function Perfil() {
           </div>
 
           <hr className="border-t border-[#266D88CC] mx-50 my-8" />
-          <h2 className="text-2xl font-bold text-[#005172] mt-2 mb-4">
-            Histórico de Doações
-          </h2>
+          <h2 className="text-2xl font-bold text-[#005172] mt-2 mb-4">Histórico de Doações</h2>
 
           <div className="mt-2 bg-white rounded-lg p-6 min-h-[580px] flex flex-col">
             <div className="flex flex-col md:flex-row gap-3 items-center mb-6 w-full">
-              <Input
-                placeholder="Buscar..."
-                fullWidth
-              />
+              <Input placeholder="Buscar..." fullWidth />
 
               <div className="relative w-full md:w-1/3">
                 <select
@@ -256,7 +316,12 @@ export default function Perfil() {
                   stroke="currentColor"
                   viewBox="0 0 24 24"
                 >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M19 9l-7 7-7-7"
+                  />
                 </svg>
               </div>
 
@@ -286,7 +351,9 @@ export default function Perfil() {
                 <PaginationContent>
                   <PaginationItem>
                     <PaginationPrevious
-                      onClick={currentPage === 1 ? undefined : () => setCurrentPage(currentPage - 1)}
+                      onClick={
+                        currentPage === 1 ? undefined : () => setCurrentPage(currentPage - 1)
+                      }
                       className={currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""}
                     >
                       Anterior
@@ -306,7 +373,11 @@ export default function Perfil() {
 
                   <PaginationItem>
                     <PaginationNext
-                      onClick={currentPage === totalPages ? undefined : () => setCurrentPage(currentPage + 1)}
+                      onClick={
+                        currentPage === totalPages
+                          ? undefined
+                          : () => setCurrentPage(currentPage + 1)
+                      }
                       className={currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""}
                     >
                       Próximo
