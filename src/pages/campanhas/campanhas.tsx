3bbcca8217eefe5f8b954f7cf3b74094d2ea829b
leofvf <leofvf@outlook.com>
2025-10-11 14:55:15 -0300

    update campaing page

--- src/pages/campanhas/campanhas.tsx
+++ src/pages/campanhas/campanhas.tsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import Button from "@/components/ui/button";
-import CampaignModal from "@/pages/campanhas/components/CampaignModal";
-
 const Campanhas = () => {
-  const [open, setOpen] = useState(false);
-
   return (
     <div className="container py-10">
       <h1 className="text-3xl font-semibold mb-4">Página de Campanhas</h1>
-
-      <Button variant="primary" size="medium" onClick={() => setOpen(true)}>
-        Abrir Campanha
-      </Button>
-
-      <CampaignModal open={open} onOpenChange={setOpen} />
     </div>
   );
 };
