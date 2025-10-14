728e25366366334149f56ddbe736f3f496cb87be
RicardoGraziato <112730538+RicardoGraziato@users.noreply.github.com>
2025-10-14 10:42:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import "./App.css";
 import Navigation from "./constant";
-import { Toaster } from "sonner";
 
 function App() {
-    return (
-    <>
-      <Toaster richColors position="top-center" />
-      <Navigation />
-    </>
-  );
+  return <Navigation />;
 }
 
 export default App;
