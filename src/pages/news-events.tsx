c1ac08fa82c9dc4fd69a1ea4e1d323c0c88c277a
Jully Anne <jully.j@edu.pucrs.br>
2025-10-16 21:51:08 -0300

    troca do card utilizado na tab para o correto

--- src/pages/news-events.tsx
+++ src/pages/news-events.tsx
@@ -2,7 +2,8 @@ import { SearchBar } from "@/components/ui/search-bar";
 import Button from "@/components/ui/button";
 import PlusIcon from "@/assets/Plus.svg";
 import { Tabs } from "@/components/ui/tabs";
-import CampaignCard from "../components/ui/campaignCard/campaignCard";
+import { CampaignCardEventAndNews } from "@/components/ui/campaignCard/campaingCardEventAndNews";
+
 
 import {
   Pagination,
@@ -14,24 +15,26 @@ import {
 } from "@/components/ui/pagination";
 import { useState } from "react";
 
-interface User {
-  profileName: string;
-  role: UserRole;
-}
-
-type UserRole = "admin" | "donor";
-
-const users: User[] = [
-  { profileName: "Fulano de Tal", role: "admin" },
-  { profileName: "Maria Silva", role: "donor" },
-  { profileName: "João Santos", role: "donor" },
-  { profileName: "Ana Lima", role: "admin" },
-  { profileName: "Pedro Costa", role: "donor" },
-];
-
 
 export default function NewsEvents() {
 
+  const eventsAndNews = [
+    {
+      title: "Campanha de Doação de Roupas",
+      date: new Date(2025, 8, 15),
+      type: "news" as const,
+    },
+    {
+      title: "Evento de Adoção de Animais",
+      date: new Date(2025, 9, 25),
+      type: "event" as const,
+    },
+    {
+      title: "Mutirão de Arrecadação de Alimentos",
+      date: new Date(2025, 10, 3),
+      type: "event" as const,
+    },
+  ];
   const mockDonors = [
     {
       id: 1,
@@ -168,11 +171,222 @@ export default function NewsEvents() {
         "Campanha de Natal Solidário",
       ],
     },
-  ];
-    const [currentPage, setCurrentPage] = useState(1);
-    const cardsPerPage = 10;
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
+    {
+      id: 1,
+      donorName: "Fulano de Tal",
+      donorEmail: "email@email.com",
+      donationAmount: 1500,
+      title: "Campanha Santo Antônio",
+      raised: 1500,
+      goal: 2000,
+      memberSince: "12/08/2023",
+      campaigns: [
+        "Campanha Santo Antônio",
+        "Campanha de Aniversário 130 anos do Pão",
+        "Campanha de Natal Solidário",
+      ],
+    },
 
-     const totalPages = Math.ceil(mockDonors.length / cardsPerPage);
+  ];
+  const [currentPage, setCurrentPage] = useState(1);
+  const cardsPerPage = 10;
+
+  const totalPages = Math.ceil(mockDonors.length / cardsPerPage);
 
   return (
     <div className="w-full py-5 px-6">
@@ -180,10 +394,7 @@ export default function NewsEvents() {
         <div className="flex-1">
           <SearchBar />
         </div>
-        <Button
-          variant="quinary"
-          size="extraSmall"
-        >
+        <Button variant="quinary" size="extraSmall">
           Pesquisar
         </Button>
         <div className="flex items-center">
@@ -195,77 +406,85 @@ export default function NewsEvents() {
 
       <div>
         <Tabs tabs={["Pendentes", "Ativos", "Todas"]} variant="secondary">
-          <div className=" flex-wrap space-y-3 w-full space-x-10 justify-center">
-            {users
-              .filter((u) => u.role === "donor")
-              .map((u, index) => (
-                <CampaignCard
-                  key={index}
-                  //profileName={u.profileName}
-                  role={u.role}
-                />
-              ))}
-          </div>
-
-          <div className=" flex-wrap space-y-3 w-full space-x-10">
-            {users
-              .filter((u) => u.role === "admin")
-              .map((u, index) => (
-                <CampaignCard
-                  key={index}
-                  //campaigns={ }
-                  role={u.role}
-                />
-              ))}
-          </div>
-
-          <div className=" flex-wrap space-y-3 w-full space-x-10">
-            {users.map((u, index) => (
-              <CampaignCard
+          <div className="flex flex-col gap-3 w-full">
+            {eventsAndNews.map((item, index) => (
+              <CampaignCardEventAndNews
                 key={index}
-                //profileName={u.profileName}
-                role={u.role}
+                title={item.title}
+                date={item.date}
+                type={item.type}
+                onDelete={() => console.log(`Excluir: ${item.title}`)}
+                onEdit={() => console.log(`Editar: ${item.title}`)}
+              />
+            ))}
+          </div>
+
+          <div className="flex flex-col gap-3 w-full">
+            {eventsAndNews
+              .filter((e) => e.type === "event")
+              .map((item, index) => (
+                <CampaignCardEventAndNews
+                  key={index}
+                  title={item.title}
+                  date={item.date}
+                  type={item.type}
+                  onDelete={() => console.log(`Excluir: ${item.title}`)}
+                  onEdit={() => console.log(`Editar: ${item.title}`)}
+                />
+              ))}
+          </div>
+
+          <div className="flex flex-col gap-3 w-full">
+            {eventsAndNews.map((item, index) => (
+              <CampaignCardEventAndNews
+                key={index}
+                title={item.title}
+                date={item.date}
+                type={item.type}
+                onDelete={() => console.log(`Excluir: ${item.title}`)}
+                onEdit={() => console.log(`Editar: ${item.title}`)}
               />
             ))}
           </div>
         </Tabs>
       </div>
+
       <div className="flex justify-center items-center gap-2 mt-6">
-      <Pagination>
-        <PaginationContent className="text-white">
-          <PaginationItem>
-            <PaginationPrevious
-              onClick={currentPage === 1 ? undefined : () => setCurrentPage(currentPage - 1)}
-              className={currentPage === 1 ? "opacity-30 cursor-not-allowed" : ""}
-            >
-              Anterior
-            </PaginationPrevious>
-          </PaginationItem>
-
-          {Array.from({ length: totalPages }, (_, i) => (
-            <PaginationItem key={i}>
-              <PaginationLink
-                onClick={() => setCurrentPage(i + 1)}
-                isActive={currentPage === i + 1}
+        <Pagination>
+          <PaginationContent className="text-white">
+            <PaginationItem>
+              <PaginationPrevious
+                onClick={currentPage === 1 ? undefined : () => setCurrentPage(currentPage - 1)}
+                className={currentPage === 1 ? "opacity-30 cursor-not-allowed" : ""}
               >
-                {i + 1}
-              </PaginationLink>
+                Anterior
+              </PaginationPrevious>
             </PaginationItem>
-          ))}
 
-          <PaginationItem>
-            <PaginationNext
-              onClick={
-                currentPage === totalPages ? undefined : () => setCurrentPage(currentPage + 1)
-              }
-              className={currentPage === totalPages ? "opacity-30 cursor-not-allowed" : ""}
-            >
-              Próximo
-            </PaginationNext>
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+            {Array.from({ length: totalPages }, (_, i) => (
+              <PaginationItem key={i}>
+                <PaginationLink
+                  onClick={() => setCurrentPage(i + 1)}
+                  isActive={currentPage === i + 1}
+                >
+                  {i + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+            <PaginationItem>
+              <PaginationNext
+                onClick={
+                  currentPage === totalPages ? undefined : () => setCurrentPage(currentPage + 1)
+                }
+                className={currentPage === totalPages ? "opacity-30 cursor-not-allowed" : ""}
+              >
+                Próximo
+              </PaginationNext>
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </div>
     </div>
   );
-};
+}
